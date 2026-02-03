85e4fcb4e1dd3aa4c362eb4a994ff8e0c53d63f5
Rahul Bhatnagar <rahulbhatnagar364@gmail.com>
2026-02-03 16:02:09 +0530

    frontend changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,7 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-app.options("*", cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
