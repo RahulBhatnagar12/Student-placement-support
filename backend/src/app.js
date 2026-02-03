f6ef3441c9a5ca2e8e98ac98d5102c3487b49f9c
Rahul Bhatnagar <rahulbhatnagar364@gmail.com>
2026-02-03 23:10:10 +0530

    backend done

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,10 +30,11 @@ app.use(
     },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+     credentials: true,
   })
 );
 
-app.options("*", cors());
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
